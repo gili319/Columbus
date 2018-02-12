52a9c8ebb43fd496748dc26f13cd2e7f5fc24d6b
gilorisr <gilorisr@post.bgu.ac.il>
2018-02-12 12:20:14 +0200

    First commit

---
+++ Assets/ShipMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour {
+
+	// Use this gggfor initialization
+	void Start () {
+		print ("yo im a ship");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Input.GetAxis("Horizontal")*Time.deltaTime, Input.GetAxis("Vertical")*Time.deltaTime, 0f);
+		//transform.Translate = new vector3(mathf.clamp(Time.time, 1.0F, 3.0F), 0, 0);
+	}
+}
